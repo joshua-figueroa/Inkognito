11d74380f619ee6c014f1b8d04ac7b2d74a5059f
Joshua Figueroa <joshuavillartafigueroa@gmail.com>
2026-05-26 23:28:45 +0800

    feat: replace SwiftData scaffold with menu-bar-hosted SwiftUI app entry

---
+++ Inkognito/App/InkognitoApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct InkognitoApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
+    var body: some Scene {
+        Settings {
+            EmptyView()
+        }
+    }
+}
